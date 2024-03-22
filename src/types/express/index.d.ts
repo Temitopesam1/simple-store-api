bb73f9b6cbb434237d10bd398bae1e7e87db3949
Temitopesam1 <temitopesam5@gmail.com>
2024-03-22 21:19:25 +0100

    modified:   src/types/express/index.d.ts

--- src/types/express/index.d.ts
+++ src/types/express/index.d.ts
@@ -1,7 +1,15 @@
 import { Types } from 'mongoose';
 
-declare module 'express' {
-    interface Request {
-        id?: Types.ObjectId;
+// declare module 'express' {
+//     interface Request {
+//         id?: Types.ObjectId;
+//     }
+// }
+
+declare global {
+    namespace Express {
+        interface Request {
+            id?: Types.ObjectId;
+        }
     }
 }
